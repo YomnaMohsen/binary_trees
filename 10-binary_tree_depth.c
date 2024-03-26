5631674c559892b0b88f651d73013d6cb39602d5
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-03-26 21:21:13 +0200

    task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -12,7 +12,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		depth = tree->parent ? 1 + binary_tree_depth(tree->parent->parent) : 0;
+		depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
 		return (depth);
 	}
 	return (0);
