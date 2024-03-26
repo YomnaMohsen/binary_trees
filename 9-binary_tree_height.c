a3ad6e27f5b520401bbe50fb0fb3102ab8937343
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-03-26 20:21:00 +0200

    task 9 fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_left, height_right;
 
-	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_left > height_right ? height_left : height_right);
+	if (tree)
+	{
+		height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return (height_left > height_right ? height_left : height_right);
+	}
+	return (0);
 }
