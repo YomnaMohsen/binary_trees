256ccdc7d9bea974ed0fbd76f4714ac2b218d87c
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-03-27 23:28:39 +0200

    task 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,32 @@
+#include"binary_trees.h"
+/**
+ * check_is_full - recuersive helper fn
+ *@tree:  a pointer to the root node of tree to measure balance factor
+* Return: 1 if balanced or 0
+*/
+int check_is_full(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if ((!tree->right && tree->left) ||
+		   (tree->right && !tree->left) ||
+		   check_is_full(tree->left) == 0 ||
+		   check_is_full(tree->right) == 0)
+			return (0);
+    }
+	return (1);
+}
+/**
+*binary_tree_is_full - checks if a binary tree is full
+*@tree:  a pointer to the root node of tree to measure balance factor
+*Return: 1 if balanced or 0
+*Description: if tree null return 0
+*/
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return (0);
+	}
+	return (check_is_full(tree));
+}
