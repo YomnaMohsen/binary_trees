3ef0aa196276fcb26000b39f98c5bcc50bc56b31
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-03-28 01:54:56 +0200

    task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,15 +2,21 @@
 
 /**
 *binary_tree_uncle -  finds the uncle of a node
-*@tree:   a pointer to the node to find the uncle
+*@node:   a pointer to the node to find the uncle
 *Return: 1 if balanced or 0
 *Description: If node is NULL, return NULL
 *             If node has no uncle, return NULL
 */
-binary_tree_t *binary_tree_uncle(const binary_tree_t *tree)
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!tree)
+	if (!node || !node->parent ||
+		!node->parent->parent)
 	{
-		return (0);
+		return (NULL);
 	}
+	if (node->parent->parent->left == node->parent)
+	{
+		return (node->parent->parent->right);
+	}
+	return (node->parent->parent->left);
 }
