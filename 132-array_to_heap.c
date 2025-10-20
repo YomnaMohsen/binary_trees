3abae135bb39699e8f1ada3be337d41808694226
YomnaMohsen <ymn_mohsen@yahoo.com>
2025-10-20 22:29:37 +0300

    fix

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -8,15 +8,15 @@
 heap_t *array_to_heap(int *array, size_t size)
  {
     size_t i;
-    heap_t *root;
+    heap_t *root = NULL;
 
     if(!array)
     {
         return(NULL);
     }
 
-    root = heap_insert(&root, array[0]);
-    for(i=1; i<size; i++)
+   /* root = heap_insert(&root, array[0]);*/
+    for(i=0; i<size; i++)
     {
         heap_insert(&root, array[i]);
     }
