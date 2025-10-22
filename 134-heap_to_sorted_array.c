8f28eff5cb393796ee0f5620c748e20150759bc0
YomnaMohsen <ymn_mohsen@yahoo.com>
2025-10-22 21:33:44 +0300

    fix

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -43,7 +43,7 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size)
         return (NULL);
     }
 
-    for(i = 0; heap ; i++)
+    for(i=0; heap ; i++)
     {
         arr[i] = heap_extract(&heap);
     }
