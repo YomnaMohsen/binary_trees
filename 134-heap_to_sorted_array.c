93d344844a0ddd59b1f6483161265954cd8ba43b
YomnaMohsen <ymn_mohsen@yahoo.com>
2025-10-22 21:28:30 +0300

    fix

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -33,11 +33,15 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size)
     int *arr, i;
     if(!heap || !size)
     {
-        return NULL;
+        return (NULL);
     }
     *size = binary_tree_size_h(heap);
 
     arr = malloc(sizeof(int) *(*size));
+    if(!arr)
+    {
+        return (NULL);
+    }
 
     for(i = 0; heap ; i++)
     {
