6775cf0d460b6864806be80ff652f0aa84793219
YomnaMohsen <ymn_mohsen@yahoo.com>
2025-10-22 21:56:12 +0300

    second

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,52 +1,53 @@
-#include"binary_trees.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "binary_trees.h"
 
 /**
-*binary_tree_size - measures the size of a binary tree
-*@tree:  a pointer to the root node of tree to measure size
-*Return: int size
-*Description: if tree null return 0
-*/
-size_t binary_tree_size_h(const binary_tree_t *tree)
+ * tree_size - measures the sum of heights of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: Height or 0 if tree is NULL
+ */
+size_t tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-	if (tree)
-	{
-		size++;
-		size += binary_tree_size(tree->right);
-		size += binary_tree_size(tree->left);
-		return (size);
-	}
-	return (0);
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		height_l = 1 + tree_size(tree->left);
+
+	if (tree->right)
+		height_r = 1 + tree_size(tree->right);
+
+	return (height_l + height_r);
 }
 
-/** 
-*heap__to_sorted_array - converts a Binary Max Heap to a sorted array of integers
-*@heap: a pointer to the root node of heap
-*@size: an address to store the size of the array.
-*Return: pointer to sorted array
-*/
+/**
+ * heap_to_sorted_array - converts a Binary Max Heap
+ * to a sorted array of integers
+ * @heap: a pointer to the root node of the heap to convert
+ * @size: an address to store the size of the array
+ *
+ * Return: the generated array
+ *         NULL on failure
+ */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-    int *arr, i;
-    if(!heap || !size)
-    {
-        return (NULL);
-    }
-    *size = binary_tree_size_h(heap);
+	int i, *a = NULL;
 
-    arr = malloc(sizeof(int) *(*size));
-    if(!arr)
-    {
-        return (NULL);
-    }
+	if (!heap || !size)
+		return (NULL);
 
-    for(i=0; heap ; i++)
-    {
-        arr[i] = heap_extract(&heap);
-    }
+	*size = tree_size(heap) + 1;
 
-    return arr;
+	a = malloc(sizeof(int) * (*size));
+
+	if (!a)
+		return (NULL);
+
+	for (i = 0; heap; i++)
+		a[i] = heap_extract(&heap);
+
+	return (a);
 }
